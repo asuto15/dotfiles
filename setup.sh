a19286043750a8ab9b8a2711cc5c1ef55b1764a0
asuto15 <sluggard0115@gmail.com>
2023-06-21 08:04:14 +0900

    add: .bashrc

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ cd $DOT_DIR
 echo "install on $(uname -n)"
 case $(uname -n) in
   "UbuntuonWSL2onG3")
+    echo "make static link .bashrc"
+    ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
     echo "make static link nvim config"
     ln -sf $HOME/dotfiles/.config/nvim/init.vim $HOME/.config/nvim/init.vim
     ln -sf $HOME/dotfiles/.config/nvim/dein.vim $HOME/.config/nvim/dein.vim
