0eb39a56ce2ad194732f9b8995d96a5d52535f35
asuto15 <sluggard0115@gmail.com>
2023-06-21 11:52:51 +0900

    update: setup on kernel-hack host

--- setup.sh
+++ setup.sh
@@ -10,9 +10,19 @@ case $(uname -n) in
     echo "make static link .bashrc"
     ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
     echo "make static link nvim config"
+    mkdir $HOME/.config/nvim
     ln -sf $HOME/dotfiles/.config/nvim/init.vim $HOME/.config/nvim/init.vim
     ln -sf $HOME/dotfiles/.config/nvim/dein.vim $HOME/.config/nvim/dein.vim
     ln -sf $HOME/dotfiles/.config/nvim/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
+    ;;
+  "KernelHackHost")
+    echo "make static link .bashrc"
+    ln -sb $HOME/dotfiles/.bashrc $HOME/.bashrc
+    echo "make static link nvim config"
+    mkdir $HOME/.config/nvim
+    ln -sf $HOME/dotfiles/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+    ln -sf $HOME/dotfiles/.config/nvim/dein.vim $HOME/.config/nvim/dein.vim
+    ln -sf $HOEM/dotfiles/.config/nvim/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
     ;;
   *)
     ;;
